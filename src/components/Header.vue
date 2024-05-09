37e8f3fbb6a1e63dd276d23970f3fc291090fbec
rel1se <botoevslava27032003@mail.ru>
2024-05-09 15:43:04 +0300

    Changed items and design

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -26,9 +26,8 @@ const handleNavigation = async (route) => {
   <header class="flex justify-between border-b border-slate-200 px-10 py-8">
     <router-link to="/">
       <div class="flex items-center gap-4">
-        <img class="w-10" src="/logo.png" alt="Logo" />
         <div>
-          <h2 class="text-xl font-bold uppercase">VUE SNEAKERS</h2>
+          <h2 class="text-xl font-black uppercase">VUE SNEAKERS</h2>
           <p class="text-slate-500">Магазин лучших кроссовок</p>
         </div>
       </div>
